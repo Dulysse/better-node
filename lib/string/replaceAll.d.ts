a875d37c575efae851868be283a070dfb18cdf3c
Ulysse Dupont <ulysse@euranov.com>
2022-03-31 16:57:19 +0200

    patch

--- lib/string/replaceAll.d.ts
+++ lib/string/replaceAll.d.ts
@@ -2,7 +2,7 @@ declare global {
   interface String {
     /**
      * `From @dulysse1/better-node`
-     *  A object can search for and replace matches within a string.
+     *  ### A object can search for and replace matches within a string.
      *  Replaces text in a string, using an object that supports replacement within a string.
      * @param searchValue search a value to replace
      * @param replaceValue replace this value by this
