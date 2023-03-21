e5c5d1fd14706252353a3e42425939b823d7a61d
Dulysse <292878@supinfo.com>
2023-03-21 17:23:05 +0100

    fix typing

--- lib/string/colors.d.ts
+++ lib/string/colors.d.ts
@@ -1,128 +1,128 @@
 interface String {
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string bolded
 	 */
 	bolded: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string dim
 	 */
 	dim: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string italic
 	 */
 	italic: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string underline
 	 */
 	underline: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string blink
 	 */
 	blink: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string inverse
 	 */
 	inverse: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string hidden
 	 */
 	hidden: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string black
 	 */
 	black: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string red
 	 */
 	red: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string green
 	 */
 	green: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string yellow
 	 */
 	yellow: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string blue
 	 */
 	blue: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string magenta
 	 */
 	magenta: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string cyan
 	 */
 	cyan: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string white
 	 */
 	white: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string crimson
 	 */
 	crimson: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background black
 	 */
 
 	bg_black: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background red
 	 */
 	bg_red: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background blue
 	 */
 	bg_green: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background black
 	 */
 	bg_yellow: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background black
 	 */
 	bg_blue: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background magenta
 	 */
 	bg_magenta: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background cyan
 	 */
 	bg_cyan: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background white
 	 */
 	bg_white: string;
 	/**
 	 * @from `@dulysse1/better-node`
-	 * ### Customize your log color
+	 * ### Make your string background crimson
 	 */
 	bg_crimson: string;
 }
